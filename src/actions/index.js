d94eedd0a72984c421eecb6dbe0b5cae0d477b53
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-11-24 19:34:09 -0300

    group-16 req05 incompleto

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,19 @@
+import { getToken } from '../services/funcs';
+
 export const ADD_QUESTIONS = 'ADD_QUESTIONS';
+export const NEXT_QUESTIONS = 'NEXT_QUESTION';
 
 export const addQuestions = (questions) => ({
   type: ADD_QUESTIONS,
   questions,
 });
+
+export const nextQuestion = () => ({ type: NEXT_QUESTIONS });
+
+export const getQuestions = () => async (dispatch) => {
+  const token = getToken();
+  const questions = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)
+    .then((res) => res.json())
+    .then((res) => res.results);
+  dispatch(addQuestions(questions));
+};
--- src/services/funcs.js
+++ src/services/funcs.js
@@ -31,10 +31,31 @@ export const getImage = async (email) => {
   const image = await fetch(`https://www.gravatar.com/avatar/${hash}`)
     .then((res) => res.url);
   return image;
-};
+}
 
 export const getLocalStorageItens = () => {
   const userStr = localStorage.getItem('state');
   const user = JSON.parse(userStr);
   return user;
 };
+
+export const getQuestionAndAnswers = async () => {
+  const amount = 5;
+  const token = getToken();
+  const data = await fetch(`https://opentdb.com/api.php?amount=${amount}&token=${token}`)
+    .then((response) => response.json());
+  return data;
+};
+
+export const shuffleArray = (array) => array.sort(() => '0.5' - Math.random());
+
+export const secondShuffleArray = (a) => {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
