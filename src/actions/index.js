185b739fa4feff8ba6de6d65f2b0c97a3732ec1a
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-11-24 17:38:03 -0300

    changes

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export const ADD_QUESTIONS = 'ADD_QUESTIONS';
+
+export const addQuestions = (questions) => ({
+  type: ADD_QUESTIONS,
+  questions,
+});
--- src/pages/Gaming.js
+++ src/pages/Gaming.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Gaming extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div>
         <Header />
+        <div className="question-card-game">
+          <p data-testid="question-category">category</p>
+          <p data-test-id="question-text">question</p>
+        </div>
+        <div className="answer-card-game">
+          <
+        </div>
       </div>
     );
   }
