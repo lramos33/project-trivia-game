259e808899d5bc11144eb34e59d92ef653e4fe16
Júnior Zanoni <zanoni.odenir@gmail.com>
2021-11-26 11:08:54 -0300

    dd

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
-      <div data-testid="ranking-title" />z
+      <div data-testid="ranking-title">
+        <Link to="/">
+          <button
+            data-testid="btn-go-home"
+            type="button"
+          >
+            Início
+          </button>
+        </Link>
+      </div>
     );
   }
 }
